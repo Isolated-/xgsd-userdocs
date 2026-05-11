5bbe0549746ac20e89cdd517c72aa5e0cb972564
Michael Palmer <michaelpalmer53@gmail.com>
2026-05-11 11:55:28 +0100

    fix url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://docs.xgsd.io",
+  url: "https://Isolated-.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
